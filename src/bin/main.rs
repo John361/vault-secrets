c12a4bbc69c9ce60d12b06ac6140e6ea3649c8e4
John H <hxjcom1@gmail.com>
2026-08-24 18:07:05 +0400

    Adding tests for main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+
 use lib_vault_secrets::cli::{Cli, Commands};
 use lib_vault_secrets::config::AppConfig;
 use lib_vault_secrets::vault::{RealVaultProvider, VaultClient};
@@ -6,18 +8,27 @@ use lib_vault_secrets::vault::{RealVaultProvider, VaultClient};
 async fn main() {
     init_tracing();
 
-    let cli = Cli::load();
-    let config = AppConfig::load(&cli.config).unwrap();
+    if let Err(e) = run().await {
+        eprintln!("Error: {e:?}");
+        std::process::exit(1);
+    }
+}
 
-    let vault_provider = RealVaultProvider::new(&config.vault).await.unwrap();
+async fn run() -> Result<()> {
+    let cli = Cli::load();
+    let config = AppConfig::load(&cli.config)?;
+
+    let vault_provider = RealVaultProvider::new(&config.vault).await?;
     let vault_client = VaultClient::new(vault_provider, config.vault.mount);
 
     match cli.command {
         Commands::Find(args) => {
-            let result = vault_client.find(&args.path, &args.key).await.unwrap();
-            println!("{result:}")
+            let result = vault_client.find(&args.path, &args.key).await?;
+            println!("{result:}");
         }
     }
+
+    Ok(())
 }
 
 fn init_tracing() {
@@ -33,4 +44,15 @@ fn init_tracing() {
         .init();
 
     tracing::debug!("Tracing initialized");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::panic;
+
+    #[test]
+    fn test_init_tracing() {
+        let _ = panic::catch_unwind(init_tracing);
+    }
 }
