a989d7de33499d5e2a13472de6f7472537807cc4
John H <hxjcom1@gmail.com>
2026-08-25 21:21:49 +0400

    #7: separate businesses for readability

--- src/lib/vault/business/export.rs
+++ src/lib/vault/business/export.rs
@@ -1,6 +1,4 @@
-use std::io::Read;
-use std::ops::Deref;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 
@@ -8,26 +6,6 @@ use crate::cli::FormatArgs;
 use crate::vault::VaultConfig;
 use crate::vault::client::VaultClient;
 use crate::vault::model::VaultData;
-
-pub struct VaultFindBusiness {
-    client: VaultClient,
-}
-
-impl VaultFindBusiness {
-    pub async fn new(config: &VaultConfig, encoded: bool) -> Result<Self> {
-        let client = VaultClient::new(config, encoded).await?;
-        Ok(Self { client })
-    }
-
-    pub async fn find(&self, mount: &str, path: &str, key: &str) -> Result<String> {
-        let result = self
-            .client
-            .find(mount, path, key)
-            .await
-            .inspect(|_| tracing::debug!("Secret for {key} found at {path}"))?;
-        Ok(result.deref().to_string())
-    }
-}
 
 pub struct VaultExportBusiness {
     client: VaultClient,
@@ -124,66 +102,6 @@ impl VaultExportBusiness {
         if !folder.exists() {
             std::fs::create_dir_all(folder)
                 .with_context(|| format!("Could not create output folder {}", folder.display()))?;
-        }
-
-        Ok(())
-    }
-}
-
-pub struct VaultImportBusiness {
-    client: VaultClient,
-}
-
-impl VaultImportBusiness {
-    pub async fn new(config: &VaultConfig, encoded: bool) -> Result<Self> {
-        let client = VaultClient::new(config, encoded).await?;
-        Ok(Self { client })
-    }
-
-    pub async fn import(
-        &self,
-        mount: &str,
-        input_folder: &Path,
-        input_format: &FormatArgs,
-    ) -> Result<()> {
-        self.check_folder(input_folder)?;
-
-        let extension = match input_format {
-            FormatArgs::Json => "json",
-            FormatArgs::Yaml => "yaml",
-        };
-
-        let file_path = input_folder.join(format!("{mount}.{extension}"));
-        let mut file_content = String::new();
-
-        std::fs::File::open(&file_path)
-            .with_context(|| format!("Failed to open file: {file_path:?}"))?
-            .read_to_string(&mut file_content)
-            .with_context(|| format!("Failed to read file content: {file_path:?}"))?;
-
-        let data: Vec<VaultData> = match input_format {
-            FormatArgs::Json => serde_json::from_str(&file_content)
-                .with_context(|| format!("Failed to parse json content: {file_path:?}"))?,
-
-            FormatArgs::Yaml => yaml_serde::from_str(&file_content)
-                .with_context(|| format!("Failed to parse yaml content: {file_path:?}"))?,
-        };
-
-        self.import_data(mount, data).await
-    }
-
-    async fn import_data(&self, mount: &str, _data: Vec<VaultData>) -> Result<()> {
-        tracing::debug!("Secrets imported to {mount}");
-        Ok(())
-    }
-
-    fn check_folder(&self, folder: &Path) -> Result<()> {
-        if folder.exists() && !folder.is_dir() {
-            anyhow::bail!("Input folder already exist and is not a directory");
-        }
-
-        if !folder.exists() {
-            anyhow::bail!("Input folder does not exist");
         }
 
         Ok(())
---
+++ src/lib/vault/business/import.rs
@@ -0,0 +1,69 @@
+use std::io::Read;
+use std::path::Path;
+
+use anyhow::{Context, Result};
+
+use crate::cli::FormatArgs;
+use crate::vault::VaultConfig;
+use crate::vault::client::VaultClient;
+use crate::vault::model::VaultData;
+
+pub struct VaultImportBusiness {
+    client: VaultClient,
+}
+
+impl VaultImportBusiness {
+    pub async fn new(config: &VaultConfig, encoded: bool) -> Result<Self> {
+        let client = VaultClient::new(config, encoded).await?;
+        Ok(Self { client })
+    }
+
+    pub async fn import(
+        &self,
+        mount: &str,
+        input_folder: &Path,
+        input_format: &FormatArgs,
+    ) -> Result<()> {
+        self.check_folder(input_folder)?;
+
+        let extension = match input_format {
+            FormatArgs::Json => "json",
+            FormatArgs::Yaml => "yaml",
+        };
+
+        let file_path = input_folder.join(format!("{mount}.{extension}"));
+        let mut file_content = String::new();
+
+        std::fs::File::open(&file_path)
+            .with_context(|| format!("Failed to open file: {file_path:?}"))?
+            .read_to_string(&mut file_content)
+            .with_context(|| format!("Failed to read file content: {file_path:?}"))?;
+
+        let data: Vec<VaultData> = match input_format {
+            FormatArgs::Json => serde_json::from_str(&file_content)
+                .with_context(|| format!("Failed to parse json content: {file_path:?}"))?,
+
+            FormatArgs::Yaml => yaml_serde::from_str(&file_content)
+                .with_context(|| format!("Failed to parse yaml content: {file_path:?}"))?,
+        };
+
+        self.import_data(mount, data).await
+    }
+
+    async fn import_data(&self, mount: &str, _data: Vec<VaultData>) -> Result<()> {
+        tracing::debug!("Secrets imported to {mount}");
+        Ok(())
+    }
+
+    fn check_folder(&self, folder: &Path) -> Result<()> {
+        if folder.exists() && !folder.is_dir() {
+            anyhow::bail!("Input folder already exist and is not a directory");
+        }
+
+        if !folder.exists() {
+            anyhow::bail!("Input folder does not exist");
+        }
+
+        Ok(())
+    }
+}
