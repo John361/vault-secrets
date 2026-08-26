d390d6951340b4b8866221d4424442a0a1070153
John H <hxjcom1@gmail.com>
2026-08-26 23:13:36 +0400

    #28: moving tests and constructing new config

--- src/lib/vault/config/general.rs
+++ src/lib/vault/config/general.rs
@@ -41,66 +41,6 @@ mod tests {
     }
 
     #[test]
-    fn test_deserialize_vault_config_username_password() {
-        let json = r#"
-        {
-            "address": "http://localhost:8200",
-            "username": "admin",
-            "password": "changeme",
-            "request_interval_ms": 200,
-            "encryption_passphrase": "my_secret_passphrase"
-        }
-        "#;
-
-        let config: VaultConfig = serde_json::from_str(json).unwrap();
-        assert_eq!(config.password.unwrap().deref(), "changeme");
-    }
-
-    #[test]
-    fn test_deserialize_vault_config_token() {
-        let json = r#"
-        {
-            "address": "http://localhost:8200",
-            "token": "changeme",
-            "request_interval_ms": 200,
-            "encryption_passphrase": "my_secret_passphrase"
-        }
-        "#;
-
-        let config: VaultConfig = serde_json::from_str(json).unwrap();
-
-        assert_eq!(config.token.unwrap().deref(), "changeme");
-    }
-
-    #[test]
-    fn test_password_is_secret_type() {
-        let config = VaultConfig {
-            address: "http://localhost:8200".to_string(),
-            username: Some("admin".to_string()),
-            password: Some(Secret::new("top_secret".to_string())),
-            token: None,
-            request_interval_ms: 200,
-            encryption_passphrase: Secret::new("my_secret_passphrase".to_string()),
-        };
-
-        assert_eq!(config.password.unwrap().deref(), "top_secret");
-    }
-
-    #[test]
-    fn test_token_is_secret_type() {
-        let config = VaultConfig {
-            address: "http://localhost:8200".to_string(),
-            username: Some("admin".to_string()),
-            password: None,
-            token: Some(Secret::new("top_secret".to_string())),
-            request_interval_ms: 200,
-            encryption_passphrase: Secret::new("my_secret_passphrase".to_string()),
-        };
-
-        assert_eq!(config.token.unwrap().deref(), "top_secret");
-    }
-
-    #[test]
     fn test_deserialize_vault_config_missing_field() {
         let json = r#"
         {
