0e99c8d0ba4e9e7b971cf0cfa6756ab2407cbbe0
John H <hxjcom1@gmail.com>
2026-08-28 22:35:25 +0400

    #20: create kv1 client

---
+++ src/lib/vault/client/client_kv1.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+
+use anyhow::{Context, Result};
+use vaultrs::kv1;
+
+use crate::secret::Secret;
+use crate::vault::VaultConnectionConfig;
+use crate::vault::client::VaultClientTrait;
+use crate::vault::model::VaultData;
+
+pub struct VaultClientKv1 {
+    client: vaultrs::client::VaultClient,
+    encode: bool,
+    request_interval_ms: u64,
+}
+
+impl VaultClientKv1 {
+    pub async fn new(
+        connection: VaultConnectionConfig,
+        encode: bool,
+        request_interval_ms: u64,
+    ) -> Result<Self> {
+        let client = Self::build_client(connection).await?;
+
+        Ok(Self {
+            client,
+            encode,
+            request_interval_ms,
+        })
+    }
+}
+
+impl VaultClientTrait for VaultClientKv1 {
+    async fn find_all(&self, mount: &str, path: &str) -> Result<HashMap<String, Secret>> {
+        let raw_results: HashMap<String, String> = kv1::get(&self.client, mount, path)
+            .await
+            .with_context(|| format!("Error reading path {path}"))?;
+        let results = Self::encode_secrets(raw_results, self.encode);
+
+        Self::sleep(self.request_interval_ms).await;
+        Ok(results)
+    }
+
+    async fn find_all_metadata(&self, _: &str, _: &str) -> Result<HashMap<String, Secret>> {
+        Ok(HashMap::new())
+    }
+
+    async fn list_paths(&self, mount: &str, path: &str) -> Result<Vec<String>> {
+        let result = kv1::list(&self.client, mount, path)
+            .await
+            .with_context(|| format!("Error listing path {path}"))?;
+
+        Self::sleep(self.request_interval_ms).await;
+        Ok(result.data.keys)
+    }
+
+    async fn set_all(&self, mount: &str, mut data_list: Vec<VaultData>) -> Result<()> {
+        self.create_mount_if_not_exists(&self.client, mount).await?;
+
+        for item in data_list.iter_mut() {
+            let data = Self::decode_secrets(item.data.clone(), self.encode)?;
+            let data: HashMap<&str, String> =
+                data.iter().map(|(k, v)| (k.as_str(), v.clone())).collect();
+
+            kv1::set(&self.client, mount, &item.path, &data).await?;
+            Self::sleep(self.request_interval_ms).await;
+        }
+
+        Ok(())
+    }
+
+    async fn set_all_metadata(&self, _: &str, _: Vec<VaultData>) -> Result<()> {
+        Ok(())
+    }
+}
--- src/lib/vault/client/mod.rs
+++ src/lib/vault/client/mod.rs
@@ -1,5 +1,7 @@
+mod client_kv1;
 mod client_kv2;
 mod client_trait;
 
+pub use client_kv1::*;
 pub use client_kv2::*;
 pub use client_trait::*;
